6b548009be67d4934be44f98aede3699fa25d4dc
angelct <angel.cardenastrujill@mavs.uta.edu>
2022-04-30 17:34:32 -0500

    Created plant subpage with grown plants xml files

---
+++ app/src/main/java/com/example/hponx/plant_tomato.java
@@ -0,0 +1,14 @@
+package com.example.hponx;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class plant_tomato extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_plant_tomato);
+    }
+}
---
+++ app/src/main/java/com/example/hponx/plant_watermelon.java
@@ -0,0 +1,14 @@
+package com.example.hponx;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class plant_watermelon extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_plant_watermelon);
+    }
+}
